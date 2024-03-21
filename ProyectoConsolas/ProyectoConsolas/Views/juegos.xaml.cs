e9255e26ba1cac7454885121e25d8cb7ca60b1ff
DAVID11 <93211987+David11A@users.noreply.github.com>
2024-03-21 16:00:15 -0600

    commit

--- ProyectoConsolas/ProyectoConsolas/Views/juegos.xaml.cs
+++ ProyectoConsolas/ProyectoConsolas/Views/juegos.xaml.cs
@@ -19,5 +19,17 @@ namespace ProyectoConsolas
 
             this.BindingContext = new VMJuegos();
         }
-	}
+        // Método para manejar el clic en la imagen de eliminar
+        private async void OnEliminarClicked(object sender, EventArgs e)
+        {
+            // Mostrar un alert al usuario para confirmar la eliminación
+            bool eliminar = await DisplayAlert("Eliminar", "¿Estás seguro que quieres eliminar este juego?", "Sí", "No");
+
+            if (eliminar)
+            {
+                // Lógica para eliminar el elemento
+                // Aquí debes llamar al método de tu ViewModel para eliminar el elemento correspondiente
+            }
+        }
+    }
 }
